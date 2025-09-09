6bedd10bad2743e1f411158cebb915dd402225ae
Sanjay8-ht <sanjay@holithemes.com>
2025-09-09 09:30:06 +0530

    updated

--- ht-design-assets-server.php
+++ ht-design-assets-server.php
@@ -11,66 +11,62 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class HT_Design_Assets_Server {
     
     public function __construct() {
-        add_action( 'parse_request', array( $this, 'serve_assets' ) );
+        add_action( 'init', array( $this, 'add_rewrite' ) );
+        add_action( 'template_redirect', array( $this, 'serve_assets' ) );
         register_activation_hook( __FILE__, array( $this, 'activate' ) );
         register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
     }
     
+    public function add_rewrite() {
+        add_rewrite_rule( '^ht-design-assets/([^/]+)$', 'index.php?htd_asset=$matches[1]', 'top' );
+        add_rewrite_tag( '%htd_asset%', '([^&]+)' );
+    }
+
     public function activate() {
         update_option( 'htd_use_server_mode', true );
+        $this->add_rewrite();
+        flush_rewrite_rules();
     }
-    
+
     public function deactivate() {
         delete_option( 'htd_use_server_mode' );
+        flush_rewrite_rules();
     }
-    
+
     public function serve_assets() {
-        error_log('HT Design Assets Server: Hook fired. REQUEST_URI: ' . $_SERVER['REQUEST_URI']);
-        
-        if ( strpos( $_SERVER['REQUEST_URI'], '/ht-design-assets/' ) === false ) {
-            error_log('HT Design Assets Server: Not an ht-design-assets request');
+        $file = get_query_var( 'htd_asset' );
+        if ( ! $file ) {
             return;
         }
 
-        error_log('HT Design Assets Server: Processing ht-design-assets request');
-        
-        $file = basename( strtok( explode( '/ht-design-assets/', $_SERVER['REQUEST_URI'] )[1], '?' ) );
         $ext = pathinfo( $file, PATHINFO_EXTENSION );
-        
-        error_log('HT Design Assets Server: File: ' . $file . ', Extension: ' . $ext);
-
-        if ( ! in_array( $ext, ['js', 'css'] ) ) { 
-            error_log('HT Design Assets Server: Invalid file extension');
-            status_header(403); 
-            exit; 
-        }
-
-        // Try multiple possible paths
-        $paths = [
-            WP_CONTENT_DIR . '/ht-design/' . $file,
-            wp_upload_dir()['basedir'] . '/ht-design/' . $file,
-            wp_upload_dir()['basedir'] . '/ht-design-assets/' . $file
-        ];
-        
-        error_log('HT Design Assets Server: Checking paths: ' . implode(', ', $paths));
-        
-        $found_path = null;
-        foreach ($paths as $path) {
-            if (file_exists($path)) {
-                $found_path = $path;
-                error_log('HT Design Assets Server: Found file at: ' . $path);
-                break;
-            }
-        }
-        
-        if (!$found_path) {
-            error_log('HT Design Assets Server: File not found in any path');
-            status_header(404); 
+        if ( ! in_array( $ext, ['js', 'css'] ) ) {
+            status_header(403);
             exit;
         }
 
-        error_log('HT Design Assets Server: Serving file: ' . $found_path);
-        header( 'Content-Type: ' . ($ext === 'js' ? 'application/javascript' : 'text/css') );
+        $upload_dir = wp_upload_dir();
+        $paths = [
+            WP_CONTENT_DIR . '/ht-design/' . $file,
+            $upload_dir['basedir'] . '/ht-design/' . $file,
+            $upload_dir['basedir'] . '/ht-design-assets/' . $file,
+        ];
+
+        $found_path = null;
+        foreach ( $paths as $path ) {
+            if ( file_exists( $path ) ) {
+                $found_path = $path;
+                break;
+            }
+        }
+
+        if ( ! $found_path ) {
+            status_header(404);
+            exit;
+        }
+
+        status_header(200);
+        header( 'Content-Type: ' . ( $ext === 'js' ? 'application/javascript; charset=UTF-8' : 'text/css; charset=UTF-8' ) );
         header( 'Cache-Control: public, max-age=2592000' );
         readfile( $found_path );
         exit;
