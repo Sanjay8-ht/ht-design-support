afbe7c0e8cecf75230e4c6652009141b4d4cd7c5
Sanjay8-ht <sanjay@holithemes.com>
2025-09-09 09:19:46 +0530

    parse and logs

--- ht-design-assets-server.php
+++ ht-design-assets-server.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 class HT_Design_Assets_Server {
     
     public function __construct() {
-        add_action( 'template_redirect', array( $this, 'serve_assets' ) );
+        add_action( 'parse_request', array( $this, 'serve_assets' ) );
         register_activation_hook( __FILE__, array( $this, 'activate' ) );
         register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
     }
@@ -25,12 +25,25 @@ class HT_Design_Assets_Server {
     }
     
     public function serve_assets() {
-        if ( strpos( $_SERVER['REQUEST_URI'], '/ht-design-assets/' ) === false ) return;
+        error_log('HT Design Assets Server: Hook fired. REQUEST_URI: ' . $_SERVER['REQUEST_URI']);
+        
+        if ( strpos( $_SERVER['REQUEST_URI'], '/ht-design-assets/' ) === false ) {
+            error_log('HT Design Assets Server: Not an ht-design-assets request');
+            return;
+        }
 
+        error_log('HT Design Assets Server: Processing ht-design-assets request');
+        
         $file = basename( strtok( explode( '/ht-design-assets/', $_SERVER['REQUEST_URI'] )[1], '?' ) );
         $ext = pathinfo( $file, PATHINFO_EXTENSION );
+        
+        error_log('HT Design Assets Server: File: ' . $file . ', Extension: ' . $ext);
 
-        if ( ! in_array( $ext, ['js', 'css'] ) ) { status_header(403); exit; }
+        if ( ! in_array( $ext, ['js', 'css'] ) ) { 
+            error_log('HT Design Assets Server: Invalid file extension');
+            status_header(403); 
+            exit; 
+        }
 
         // Try multiple possible paths
         $paths = [
@@ -39,21 +52,24 @@ class HT_Design_Assets_Server {
             wp_upload_dir()['basedir'] . '/ht-design-assets/' . $file
         ];
         
+        error_log('HT Design Assets Server: Checking paths: ' . implode(', ', $paths));
+        
         $found_path = null;
         foreach ($paths as $path) {
             if (file_exists($path)) {
                 $found_path = $path;
+                error_log('HT Design Assets Server: Found file at: ' . $path);
                 break;
             }
         }
         
         if (!$found_path) {
-            // Debug: Log attempted paths
-            error_log('HT Design Assets Server: File not found. Tried: ' . implode(', ', $paths));
+            error_log('HT Design Assets Server: File not found in any path');
             status_header(404); 
             exit;
         }
 
+        error_log('HT Design Assets Server: Serving file: ' . $found_path);
         header( 'Content-Type: ' . ($ext === 'js' ? 'application/javascript' : 'text/css') );
         header( 'Cache-Control: public, max-age=2592000' );
         readfile( $found_path );
